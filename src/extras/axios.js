5db6d6e4f0e1a815059546eba3d67d785edbf8ac
deralph <jraphael441@gmail.com>
2022-12-06 14:53:49 -0800

    changed the server host

--- src/extras/axios.js
+++ src/extras/axios.js
@@ -4,7 +4,8 @@ export default axios.create({
   // baseURL: "http://localhost:5000/api/v1",
   // baseURL: "http://ec2-54-164-115-29.compute-1.amazonaws.com:5000/api/v1",
   // baseURL: "https://expense-tracker-backend-sigma.vercel.app/api/v1",
-  baseURL: "https://expense-tracked-01.herokuapp.com/api/v1",
+  // baseURL: "https://expense-tracked-01.herokuapp.com/api/v1",
+  baseURL: "https://expense-tracker-backend-8685.onrender.com",
   withCredentials: true,
   credentials: "include",
 });
